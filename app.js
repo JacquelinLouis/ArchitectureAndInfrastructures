d9143fab922d97373e4f6f24447f50273ddc6112
jacque_l <jacquelinlouis@gmail.com>
2017-11-27 20:25:17 +0100

    Comment mysql import

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const mysql = require('./mysql');
+// const mysql = require('./mysql');
 
 app.get('/', function(req, res) {
     res.send("GET response");
