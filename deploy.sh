168661a17cf3f6a23d0e8f191f79506c525e499d
jacque_l <jacquelinlouis@gmail.com>
2017-11-27 11:02:29 +0100

    Add cd to app folder in script

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,10 @@ apt-get update
 apt-get install nodejs
 # install yarn
 apt-get install yarn
+# clone public repository
+git clone https://github.com/JacquelinLouis/ArchitectureAndInfrastructures.git
+# move to app folder
+cd ArchitectureAndInfrastructures/
 # install Node.js server's dependencies
 yarn install
 # start server
