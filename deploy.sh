1b4fdfe0b61ff0b53d5b634e0f095356e90c5fd1
jacque_l <jacquelinlouis@gmail.com>
2017-11-27 11:21:27 +0100

    Add comments during shell script execution

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
-# set root role
-sudo su
 # update dependencies
 apt-get update
-# install Node.js
-apt-get install nodejs
-# install yarn
-apt-get install yarn
+echo 'Updated dependencies'
+# install Node.js, confirm yes
+apt-get install nodejs -y
+echo 'Installed Node.js'
+# install yarn, confirm yes
+apt-get install yarn -y
+echo 'Installed Yarn'
 # clone public repository
 git clone https://github.com/JacquelinLouis/ArchitectureAndInfrastructures.git
+echo 'Cloned repository from https://github.com/JacquelinLouis/ArchitectureAndInfrastructures.git'
 # move to app folder
 cd ArchitectureAndInfrastructures/
 # install Node.js server's dependencies
 yarn install
+echo 'Yarn install command executed'
 # start server
 yarn start
