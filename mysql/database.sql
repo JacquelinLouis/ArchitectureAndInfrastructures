19963b0d696b3ade68fcf43959971b80d7ffee61
Guillaume Audinet <audine_g@epita.fr>
2017-12-12 18:04:41 +0100

    Update mySQL script

--- mysql/database.sql
+++ mysql/database.sql
@@ -1,15 +1,15 @@
 CREATE DATABASE archi_db;
 USE archi_db;
 
-GRANT USAGE ON *.* TO user@localhost IDENTIFIED BY 'user';
-GRANT ALL PRIVILEGES ON archi_db.* TO user@localhost;
+GRANT USAGE ON *.* TO 'user'@'%' IDENTIFIED BY 'user';
+GRANT ALL PRIVILEGES ON archi_db.* TO 'user'@'%';
 FLUSH PRIVILEGES;
 
 CREATE TABLE user
 (
     id SERIAL PRIMARY KEY NOT NULL,
-    email VARCHAR(20) NOT NULL UNIQUE,
-    password VARCHAR(50) NOT NULL
+    email VARCHAR(30) NOT NULL UNIQUE,
+    password VARCHAR(30) NOT NULL
 );
 
 INSERT INTO user VALUES ('1', 'firstmail@domain.com', 'password');
