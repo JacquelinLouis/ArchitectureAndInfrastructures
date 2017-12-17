15c8f080d4ad96f77137eb5e70050fae75b0c7fa
Guillaume Audinet <audine_g@epita.fr>
2017-12-17 17:59:58 +0100

    Update Database Creation

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -1,7 +1,14 @@
 # Create database
 CREATE DATABASE TodoList;
+
 # Use database
 USE TodoList;
+
+# Create with PRIVILEGES new user
+GRANT USAGE ON *.* TO 'user'@'%' IDENTIFIED BY 'user';
+GRANT ALL PRIVILEGES ON archi_db.* TO 'user'@'%';
+FLUSH PRIVILEGES;
+
 # Create table
 CREATE TABLE Messages (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -9,3 +16,6 @@ CREATE TABLE Messages (
   message VARCHAR(1024) NOT NULL,
   date TIMESTAMP NOT NULL
 );
+
+INSERT INTO Messages VALUES ('1', 'Louis', 'Hola! Qué tal ?', NOW());
+INSERT INTO Messages VALUES ('2', 'Guillaume', 'Bonjour! Comment vas-tu ?', NOW());
