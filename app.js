e8a5c770d0f1f25749502bf533ccae6e0963d25e
jacque_l <jacquelinlouis@gmail.com>
2017-12-18 16:46:23 +0100

    Change port 3000 to default 80

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = 3000;
+const port = 80;
 const mysql = require('mysql');
 const fs = require('fs');
 
